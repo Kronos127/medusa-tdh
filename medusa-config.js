64182a8c56a2dbc1c935337103d064846be95be5
kronos127 <grigore.munteanu@gmail.com>
2024-10-05 21:03:56 +0300

    +

--- medusa-config.js
+++ medusa-config.js
@@ -95,3 +95,23 @@ module.exports = {
   plugins,
   modules,
 };
+
+const plugins = [
+  // ...
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+]
+const plugins = [
+  // ...
+  {
+    resolve: `medusa-plugin-categories`,
+    options: {
+      enableUI: true,
+    },
+  },
+];
